5721b6e08a7ff6b65523ed52773e060ab4d5c6f0
bhivam <shivamkajaria@gmail.com>
2024-04-19 19:22:27 -0400

    underlined selected tab

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,19 @@ function App() {
   ];
 
   return (
-    <div className="box-border flex h-[100vh] w-[100vw] justify-center">
-      <div className="flex  flex-col px-8 w-full md:w-3/4">
+    <div className="flex h-[100vh] w-[100vw] justify-center">
+      <div className="flex flex-col px-8 w-full md:w-3/4">
         <div className="flex flex-row">
           <div
             className={`text-xl xs:text-2xl md:text-5xl sm:text-3xl font-bold py-3 pr-4 text-black 
-                        ${tab === 0 ? "" : "text-opacity-25"}`}
+                        ${tab === 0 ? "border-solid border-0 border-b-2 border-black" : "text-opacity-25"}`}
             onClick={() => setTab(0)}
           >
             Notes
           </div>
           <div
-            className={`text-xl xs:text-2xl md:text-5xl sm:text-3xl  font-bold py-3 pr-4 text-black
-                        ${tab === 1 ? "" : "text-opacity-25"}`}
+            className={`text-xl xs:text-2xl md:text-5xl sm:text-3xl font-bold py-3 pr-4 text-black 
+                        ${tab === 1 ? "border-solid border-0 border-b-2 border-black" : "text-opacity-25"}`}
             onClick={() => setTab(1)}
           >
             Summarization
