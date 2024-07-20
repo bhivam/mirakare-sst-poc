959af050b881d22462111f9ccc50fb0deea49972
bhivam <shivamkajaria@gmail.com>
2024-07-19 23:41:25 -0400

    changed format from mpeg to mp3 for safari

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -192,7 +192,7 @@ export default function NotesPage({ user_id }: { user_id: string | null }) {
 
         recorder.current.onstop = (e: Event) => {
             console.log(e);
-            const blob: Blob = new Blob(chunks, { type: "audio/mpeg" });
+            const blob: Blob = new Blob(chunks, { type: "audio/mp3" });
 
             chunks = [];
             const audio_file: File = new File([blob], "audio_file.mp3", {
